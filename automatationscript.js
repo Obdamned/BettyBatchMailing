79f469e657f89b841848ccd80560e2ee1555413c
Cas Obdam <casobdam@gmail.com>
2017-10-25 07:03:29 -0700

    Wrapped in specific function
    So that it will loops

--- automatationscript.js
+++ automatationscript.js
@@ -1,29 +1,34 @@
-setTimeout(function(){
-  var j = document.getElementsByClassName('private-tabs__list');
-  var je = j[0].getElementsByTagName('a');
-  je[1].click();
-
+function send_email () {
   setTimeout(function(){
-    var s = document.getElementsByClassName('editor-body');
-    var se = s[0].getElementsByTagName('li');
-    se[1].childNodes[0].childNodes[0].click();
+    var j = document.getElementsByClassName('private-tabs__list');
+    var je = j[0].getElementsByTagName('a');
+    je[1].click();
 
     setTimeout(function(){
-      var p = document.getElementsByClassName('private-typeahead-results');
-      p[0].getElementsByTagName('li')[15].childNodes[0].click();
+      var s = document.getElementsByClassName('editor-body');
+      var se = s[0].getElementsByTagName('li');
+      se[1].childNodes[0].childNodes[0].click();
 
-      setTimeout(function() {
-        var c = document.getElementsByClassName('btn uiButton private-button private-button--default private-button--primary btn-primary m-right-3');
-        c[0].click();
+      setTimeout(function(){
+        var p = document.getElementsByClassName('private-typeahead-results');
+        p[0].getElementsByTagName('li')[15].childNodes[0].click();
 
         setTimeout(function() {
-          var b = document.getElementsByClassName('private-button__link')[14].click();
+          var c = document.getElementsByClassName('btn uiButton private-button private-button--default private-button--primary btn-primary m-right-3');
+          c[0].click();
 
           setTimeout(function() {
-            document.getElementsByClassName('uiLinkWithoutUnderline')[0].click()
-          }, 2000)
+            var b = document.getElementsByClassName('private-button__link')[document.getElementsByClassName('private-button__link').length - 1].click();
+            
+            setTimeout(function() {
+              document.getElementsByClassName('uiLinkWithoutUnderline')[0].click();
+              send_email ();
+            }, 2000)
+          }, 2000);
         }, 2000);
       }, 2000);
-    }, 2000);
-  }, 2000);
-}, 3000);
+    }, 1000);
+  }, 500);
+}
+
+send_email ();
