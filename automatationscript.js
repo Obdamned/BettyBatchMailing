9a1db73eadb892f504a26a846438f3069a72d98b
Cas Obdam <casobdam@gmail.com>
2017-10-25 06:05:42 -0700

    Second commit
    The full JavaScipt for automating emails WITHIN a task.

---
+++ automatationscript.js
@@ -0,0 +1,26 @@
+setTimeout(function(){
+  var j = document.getElementsByClassName('private-tabs__list');
+  var je = j[0].getElementsByTagName('a');
+  je[1].click();
+
+  setTimeout(function(){
+    var s = document.getElementsByClassName('editor-body');
+    var se = s[0].getElementsByTagName('li');
+    se[1].childNodes[0].childNodes[0].click();
+    
+    setTimeout(function(){
+      var p = document.getElementsByClassName('private-typeahead-results');
+      p[0].getElementsByTagName('li')[15].childNodes[0].click();
+
+      setTimeout(function() {
+        var c = document.getElementsByClassName('btn uiButton private-button private-button--default private-button--primary btn-primary m-right-3');
+        c[0].click();
+
+        setTimeout(function() {
+          var b = document.getElementsByClassName('btn uiButton private-button private-button--default private-button__link btn-link');
+          b[0].click();
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 1000);
